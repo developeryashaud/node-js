eb84e0fa259be793508fae04fa95cca641e71587
YASH <154601201+developeryashaud@users.noreply.github.com>
2024-02-19 02:01:35 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hello developer")
+const express=require('express')
+app=express()
+port=8080
+
+app.get('/',(req,res)=>{
+  res.send("hello world")
+})
+app.listen(port,()=>{
+  console.log("server is running")
+})
